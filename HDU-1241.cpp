2dd0691d3732f25affa45526eb48f934a31ca43e
yejichen <yejichen2@sina.cn>
2017-03-15 16:45:10 +0800

    Update HDU-1241.cpp

--- HDU-1241.cpp
+++ HDU-1241.cpp
@@ -1,18 +1,49 @@
-#include<iostream>
+#include <iostream>
+#include <stdio.h>
 using namespace std;
-char a[101][101];
+
+char map[101][101];
+int n, m, sum;
+
+void dfs(int i, int j)
+{
+    if(map[i][j]!='@' || i<0 || j<0 || i>=m || j>=n) return;    
+    else
+    {
+        map[i][j] = '!';
+        dfs(i-1, j-1);
+        dfs(i-1, j);
+        dfs(i-1, j+1);
+        dfs(i, j-1);
+        dfs(i, j+1);
+        dfs(i+1, j-1);
+        dfs(i+1, j);
+        dfs(i+1, j+1);
+    }
+}
+
 int main()
 {
-  int m,n;
-  while(cin>>m>>n&&m!=0&&n!=0)
-  {
-    ans=0;
-    getchar()
-    for(i=0;i<m;i++)
+    int i, j;
+    while(cin>>m>>n)
     {
-      for(j=0;j<n;j++)
-      {
-        getchar()=a[i][j];
-      }
-      getchar();
+        if(m==0 || n==0) break;
+        sum = 0;
+        for(i = 0; i < m; i++)
+            for(j = 0; j < n; j++)
+                cin>>map[i][j];
+        for(i = 0; i < m; i++)
+        {
+            for(j = 0; j < n; j++)
+            {
+                if(map[i][j] == '@')
+                {
+                    dfs(i, j);
+                    sum++;
+                }
+            }
+        }
+        cout<<sum<<endl;
     }
+    return 0;
+}
