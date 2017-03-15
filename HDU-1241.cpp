93b76ee640e247a118f7502c7ae7f5d74b896c6c
yejichen <yejichen2@sina.cn>
2017-03-15 12:18:16 +0800

    Update HDU-1241.cpp

--- HDU-1241.cpp
+++ HDU-1241.cpp
@@ -1,1 +1,18 @@
 #include<iostream>
+using namespace std;
+char a[101][101];
+int main()
+{
+  int m,n;
+  while(cin>>m>>n&&m!=0&&n!=0)
+  {
+    ans=0;
+    getchar()
+    for(i=0;i<m;i++)
+    {
+      for(j=0;j<n;j++)
+      {
+        getchar()=a[i][j];
+      }
+      getchar();
+    }
