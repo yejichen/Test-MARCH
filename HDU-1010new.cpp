14561957706e2c9e5c978a74f23ae95b211ecb87
yejichen <yejichen2@sina.cn>
2018-05-11 18:30:32 +0800

    Update HDU-1010new.cpp

--- HDU-1010new.cpp
+++ HDU-1010new.cpp
@@ -1,5 +1,23 @@
 #include<iostream>
 using namespace std;
-int main()
+int n,m,t;
+char map[10][10];
+int dfs(int x,int y)
 {
   
+int main()
+{
+  cin>>n>>m;
+  int i,j;
+  for(i=1;i<=n;i++)
+  {
+    for(j=1;j<=m;j++)
+    {
+      cin>>map[i][j];
+      if(map[i][j]=='S')
+      {
+         int p=i;
+         int q=j;
+      }
+    }
+  }
